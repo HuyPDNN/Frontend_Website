3ff991c731b69f603eac707625414cadf37e0c92
Duy Nguyen <duy.nguyen@miracles.vn>
2020-06-30 08:37:18 +0700

    Javascript Duy

---
+++ javascript-Duy/code-js.js
@@ -0,0 +1,26 @@
+// //khai bao bien glocal(toan cuc)
+// var a=5;
+// function tong(b,c){
+//     //khai bao bien local(cuc bo)
+//     var Tong=a+b+c;
+//     return Tong;
+// }
+// alert(tong(5,10));
+
+// //hỗ trợ xử lý chuỗi
+// var str="HELLO"
+// alert(str.length);
+
+
+// var date = new Date();
+// date.setDate(28);
+// alert(date);
+
+
+// var obj = document.getElementById("Menu");
+// var ob = obj.innerHTML;
+// alert(ob);
+
+function hello(){
+    alert("Tìm Kiếm");
+}
