d68d1522cde1044427477925547a27bdb058502b
Duy Nguyen <duy.nguyen@miracles.vn>
2020-07-01 17:40:25 +0700

    JQuery Cua Duy

---
+++ javascript-Duy/jquery.js
@@ -0,0 +1,138 @@
+//chờ tài liệu tải xong(giống onload)
+$(document).ready(function () {
+    //lệnh trong này chỉ chạy khi tài liệu tải xong
+    var bt = document.getElementById('Hinh');
+    bt.onmouseover = doihinh; 
+    bt.onclick = doiclass;
+
+    //JQuery điều khiển đọc nội dung html
+    var html = document.getElementById("dochtml");
+    html.onclick=Dochtml;
+
+    //JQuery điều khiển đổi nội dung html
+    var Html = document.getElementById("doihtml");
+    Html.onclick=Doihtml;
+    
+    //JQuery điều khiển đọc nội dung text
+    var text = document.getElementById('doctext');
+    text.onclick=Doctext;
+
+    //JQuery điều khiển đổi nội dung text
+    var Text = document.getElementById('doitext');
+    Text.onclick=Doitext;
+
+    //JQuery chèn nội dung vào sau thẻ html
+    var sau = document.getElementById('ndsau');
+    sau.onclick=chensau;
+
+    //JQuery chèn nội dung vào đầu thẻ html
+    var dau = document.getElementById('nddau');
+    dau.onclick=chendau;
+
+    //JQuery chèn nội dung vào sau trong thẻ html
+    var ndsautrong = document.getElementById('ndcuoitrong');
+    ndsautrong.onclick=ndsau;
+
+    //JQuery chèn nội dung vào sau trong thẻ html
+    var nddautrong = document.getElementById('nddautrong');
+    nddautrong.onclick=nddau;
+
+    //JQuery ẩn nội dung
+    var annd = document.getElementById('annd');
+    annd.onclick=an;
+
+    //JQuery hiện nội dung
+    var hiennd = document.getElementById('hiennd');
+    hiennd.onclick=hien;
+
+});
+
+//JQuery Atttribute--đọc nội dung và gán thuộc tính----Đổi hình//////////////////
+var HINH = 1;
+function doihinh() {
+    if (HINH == 1) {
+        $('#anh').attr("src", "./image/map.jpg");
+        HINH = 2;
+    }
+    else {
+        $('#anh').attr("src", "./image/mayanh.jpg");
+        HINH = 1;
+    }
+}
+
+//JQuery và Class------đổi Class----làm to nhỏ hình ảnh//////
+var hinh = 1 ;
+function doiclass(){
+    if(hinh==1){
+        $('#anh').addClass('w2');
+        hinh = 2;
+    }
+    else if(hinh == 2){
+        $('#anh').addClass('w3')
+        hinh = 3;
+    } else if(hinh=3){
+        $('#anh').removeClass('w2').removeClass('w3');
+        hinh = 1;
+    }
+
+    //JQuery và CSS
+    //đọc thuộc tính width trong css của id=anh
+    var css = $('#anh').css('width');
+    alert(css);
+    /*-----------------------------------------*/
+}
+
+//JQuery điều khiển nội đọc dung html
+function Dochtml(){
+    var html = document.getElementById("dochtml");
+    html = $("#nd").html();
+    alert(html);
+}
+//JQuery điều khiển đổi nội dung html
+function Doihtml(){
+    var Html = document.getElementById("doihtml");
+    Html = $("#nd").html('<b> Nội dung mới</b> <br/> <span> Nội dung span mới</span>')
+}
+//JQuery điều khiển đọc nội dung text
+function Doctext(){
+    var text = document.getElementById("doctext");
+    text = $("#nd").text();
+    alert(text);
+}
+//JQuery điều khiển đổi nội dung text
+function Doitext(){
+    var Text = document.getElementById("doitext");
+    Text = $("#nd").text('Hello');
+}
+//JQuery chèn thêm nội dung vào sau thẻ html 
+function chensau(){
+    var sau = document.getElementById('ndsau');
+    sau = $('#nd').append("Xin Chao") ;
+}     
+//JQuery chèn thêm nội dung vào sau thẻ html 
+function chendau(){
+    var dau = document.getElementById('nddau');
+    dau = $('#nd').prepend("Chao Ban");
+}   
+//JQuery chèn thêm nội dung vào sau trong thẻ html 
+function ndsau(){
+    var ndsautrong = document.getElementById('ndcuoitrong');
+    ndsautrong = $("<b>CHAO BAN</B> ").appendTo('#nd');
+} 
+//JQuery chèn thêm nội dung vào đầu trong thẻ html 
+function nddau(){
+    var nddautrong = document.getElementById('nddautrong');
+    nddautrong = $('<B>CHAO BAN</B>').prependTo('#nd');
+} 
+//JQuery ẩn và hiện nội dung
+function an(){
+    var annd = document.getElementById('annd');
+    annd = $('b').hide();
+}
+function hien(){
+    var hiennd = document.getElementById('hiennd');
+    hiennd = $('b').show();
+}
+
+
+
